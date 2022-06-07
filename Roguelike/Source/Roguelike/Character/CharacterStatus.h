b01b8e352298188953c257afb2d41e2298591910
wertrain <wertrain@gmail.com>
2022-06-08 01:43:28 +0900

    アイテムボックス UI 対応中

--- Roguelike/Source/Roguelike/Character/CharacterStatus.h
+++ Roguelike/Source/Roguelike/Character/CharacterStatus.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Roguelike/Character/CharacterDefinitions.h"
+#include "Engine/DataTable.h"
 #include "CharacterStatus.generated.h"
 
 /**
@@ -41,4 +42,17 @@ public:
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Roguelike|Character")
 	EFactions Faction;
+};
+
+USTRUCT(BlueprintType)
+struct FCharacterData : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FName Name;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FCharacterStatus Status;
 };
