94e12b059bd3e583574810834b864d1a9b0ae45e
wertrain <wertrain@gmail.com>
2022-07-03 16:12:49 +0900

    装備ファイルの削除

--- Roguelike/Source/Roguelike/Item/ItemData.h
+++ Roguelike/Source/Roguelike/Item/ItemData.h
@@ -35,3 +35,40 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	FText Summary;
 };
+
+UENUM(Blueprintable)
+enum class EEquipmentTypes : uint8
+{
+	RightHand UMETA(DisplayName = "Right Hand"),
+	LeftHand UMETA(DisplayName = "Left Hand"),
+	Accessory UMETA(DisplayName = "Accessory"),
+	Num UMETA(Hidden)
+};
+
+USTRUCT(BlueprintType)
+struct FEquipmentData : public FItemData
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EEquipmentTypes EquipmentType;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Level;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 HealthPoint;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 MagicPoint;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Strength;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Defense;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Agility;
+};
